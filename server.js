813e0a8cfc87deb3abde5abf95e3dcc67f0b9cf0
Yogita Kohli <yogitakohli12345@gmail.com>
2025-06-21 13:35:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/fleetlink', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect('mongodb://localhost:27017/fleetlink').then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("Mongo Error:", err));
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/bookings', bookingRoutes);
